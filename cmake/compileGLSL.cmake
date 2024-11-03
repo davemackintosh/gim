1210347d593a1c441c3ab9e13127d31954582255
davemackintosh <me@davemackintosh.co.uk>
2024-11-03 10:08:00 +0000

    spirv generation fixes

--- cmake/compileGLSL.cmake
+++ cmake/compileGLSL.cmake
@@ -1,29 +1,25 @@
 macro(append_glsl_to_target)
-  message("Processing shaders: ${ARGV}")
+  message("Processing shaders: ${ARGV} to ${PROJECT_BINARY_DIR}/shaders/")
   foreach(GLSL ${ARGV})
     message("Processing: ${GLSL}")
     get_filename_component(FILE_NAME ${GLSL} NAME)
     set(SPIRV "${PROJECT_BINARY_DIR}/shaders/${FILE_NAME}.spv")
 
+    message("VALIDATOR", Vulkan::glslc)
     add_custom_command(
       OUTPUT ${SPIRV}
       COMMAND cd
+      COMMAND echo "building shader: '${SPIRV}'"
       COMMAND "${CMAKE_COMMAND}" -E make_directory
               "${PROJECT_BINARY_DIR}/shaders/"
-      COMMAND "${GLSL_VALIDATOR}" -V ${GLSL} -o ${SPIRV}
+      COMMAND Vulkan::glslc "${GLSL}" "-o" "${SPIRV}"
+      BYPRODUCTS ${SPIRV}
       DEPENDS ${GLSL})
+
     list(APPEND SPIRV_BINARY_FILES ${SPIRV})
   endforeach(GLSL)
 
   add_custom_target(Shaders DEPENDS ${SPIRV_BINARY_FILES})
 
   add_dependencies(${PROJECT_NAME} Shaders)
-
-  add_custom_command(
-    TARGET ${PROJECT_NAME}
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory
-            "$<TARGET_FILE_DIR:${PROJECT_NAME}>/shaders/"
-    COMMAND ${CMAKE_COMMAND} -E copy_directory "${PROJECT_BINARY_DIR}/shaders"
-            "$<TARGET_FILE_DIR:${PROJECT_NAME}>/shaders")
 endmacro()
